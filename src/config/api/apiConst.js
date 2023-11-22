36f18ed2b7d8595046f96e858cf02860d2646377
npvdung <npvdrealmadrid@gmail.com>
2023-11-23 01:30:20 +0700

    verify email when reset pass

--- src/config/api/apiConst.js
+++ src/config/api/apiConst.js
@@ -1,18 +1,18 @@
-const baseUrl = process.env.REACT_APP_BASE_API_URL
+const baseUrl = process.env.REACT_APP_BASE_API_URL;
 
 // const baseUrl = 'http://localhost:8888'
 
 export const ApiUrl = {
-  questions: baseUrl + '/questions',
-  reading: baseUrl + '/reading',
-  exam: baseUrl + '/exams',
-  getGame: baseUrl + '/games/getGame',
-  finishGame: baseUrl + '/games/finishGame',
-  login: baseUrl + '/users/authenticate',
-  register: baseUrl + '/users/register',
-  categories: baseUrl + '/categories',
-  getTop: baseUrl + '/results/rate',
-  results: baseUrl + '/results',
-  user: baseUrl + '/users',
-  examDetail: baseUrl + '/games/exam-detail',
-}
+  questions: baseUrl + "/questions",
+  reading: baseUrl + "/reading",
+  exam: baseUrl + "/exams",
+  getGame: baseUrl + "/games/getGame",
+  finishGame: baseUrl + "/games/finishGame",
+  login: baseUrl + "/users/authenticate",
+  register: baseUrl + "/users/register",
+  categories: baseUrl + "/categories",
+  getTop: baseUrl + "/results/rate",
+  results: baseUrl + "/results",
+  user: baseUrl + "/users",
+  examDetail: baseUrl + "/games/exam-detail",
+};
--- src/pages/oauth/common/ForgotPass.jsx
+++ src/pages/oauth/common/ForgotPass.jsx
@@ -3,40 +3,66 @@ import { Form, Input } from "antd";
 import HvxButton from "../../../components/button/HvxButton";
 
 const ForgotPass = () => {
-  const [isSentCode, setIsSentCode] = useState(true);
+  const [isSentCode, setIsSentCode] = useState("default");
+  const [email, setEmail] = useState("");
+  const checkEmail = async (values) => {
+    const axios = require("axios");
+    let data = JSON.stringify({
+      email: values.email,
+    });
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: `${process.env.REACT_APP_BASE_API_URL}/users/forget-password`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          setIsSentCode("valid");
+        }
+      })
+      .catch((error) => {
+        setIsSentCode("error");
+      });
+  };
   return (
     <div>
-      <Form>
+      <Form onFinish={checkEmail}>
         <Form.Item
           style={{ marginBottom: "10px" }}
           label="Please enter your email:"
           required="true"
+          name="email"
           rules={[{ message: "Please input your email!" }]}
         >
           <Input placeholder="Email" />
         </Form.Item>
 
-        {isSentCode ? (
-          <>
-            <p style={{ color: "#94a3b8", marginBottom: "15px" }}>
-              The code has been sent to your email. Please check
-            </p>
-            <Form.Item
-              label="Please enter received code:"
-              required="true"
-              rules={[{ message: "Please input the code!" }]}
-            >
-              <Input.Password />
-            </Form.Item>
-          </>
-        ) : (
-          <p style={{ color: "#94a3b8" }}>
-            The code will be sent to your email
+        {isSentCode === "default" ? (
+          <p style={{ color: "#94a3b8", marginBottom: "15px" }}>
+            We will sent a verification link to your email.
           </p>
-        )}
+        ) : isSentCode === "error" ? (
+          <p style={{ color: "red" }}>
+            Your email is not in our system. Please try again.
+          </p>
+        ) : isSentCode === "valid" ? (
+          <p style={{ color: "green" }}>
+            We have sent a verification link to your email. Please check your
+            email.
+          </p>
+        ) : null}
 
         <div className="hvx-btn-login" style={{ marginTop: "15px" }}>
-          <HvxButton type="primary" htmlType="submit" text="Send code" />
+          <HvxButton type="primary" htmlType="submit" text="Send link" />
         </div>
       </Form>
     </div>
